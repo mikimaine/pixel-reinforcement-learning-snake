251e32101c99f4b2125afcba9fe5253d7b20f513
Luis Lobo Borobia <lml230005@utdallas.edu>
2024-07-27 00:16:17 -0500

    Add some data type validation

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFormLayout, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QFormLayout, QPushButton, QMessageBox
 
 from trainingagent import train
+
 
 class GameSettings:
     def __init__(self):
         self.settings = {
-            'speed': 20,
-            'state_size': 11,
-            'hidden_layer_size': 256,
-            'action_size': 3,
-            'max_memory': 100,
-            'epsilon_decay': 80,
-            'learning_rate': 0.001,
-            'batch_size': 1000,
-            'gamma': 0.9
+            'speed': {'value': 20, 'type': 'int'},
+            'state_size': {'value': 11, 'type': 'int'},
+            'hidden_layer_size': {'value': 256, 'type': 'int'},
+            'action_size': {'value': 3, 'type': 'int'},
+            'max_memory': {'value': 100, 'type': 'int'},
+            'epsilon_decay': {'value': 80, 'type': 'int'},
+            'learning_rate': {'value': 0.001, 'type': 'float'},
+            'batch_size': {'value': 1000, 'type': 'int'},
+            'gamma': {'value': 0.9, 'type': 'float'}
         }
+
 
 class SettingScreen(QWidget):
     def __init__(self, settings, start_game_callback):
         super().__init__()
+        self.train_agent_button = None
         self.game_settings = settings
         self.start_game_callback = start_game_callback
         self.input_fields = {}
@@ -27,9 +30,10 @@ class SettingScreen(QWidget):
 
     def ui(self):
         layout = QFormLayout()
+        self.setMinimumSize(400, 300)  # Ensure UI elements are not cramped.
 
-        for setting, value in self.game_settings.settings.items():
-            input_field = QLineEdit(str(value))
+        for setting, info in self.game_settings.settings.items():
+            input_field = QLineEdit(str(info['value']))
             self.input_fields[setting] = input_field
             layout.addRow(f'{setting.replace("_", " ").title()}', input_field)
 
@@ -42,18 +46,31 @@ class SettingScreen(QWidget):
         self.show()
 
     def train_agent(self):
+        error = False
         for setting, input_field in self.input_fields.items():
+            text_value = input_field.text()
+            setting_type = self.game_settings.settings[setting]['type']
             try:
-                self.game_settings.settings[setting] = int(input_field.text())
+                if setting_type == 'float':
+                    value = float(text_value)
+                elif setting_type == 'int':
+                    value = int(text_value)
+                else:
+                    value = text_value
+                self.game_settings.settings[setting]['value'] = value
             except ValueError:
-                input_field.setText(str(self.game_settings.settings[setting]))  # Reset to the last valid value
-                continue
+                QMessageBox.warning(self, "Invalid Input",
+                                    f"Please enter a valid number for {setting.replace('_', ' ').title()}.")
+                input_field.setText(str(self.game_settings.settings[setting]['value']))  # Reset to the last valid value
+                error = True
+        if not error:
+            self.start_game_callback(self.game_settings)
 
-        self.start_game_callback(self.game_settings)
 
 def train_agent(game_settings):
     print(f'Game settings: {game_settings.settings}')
     train()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
